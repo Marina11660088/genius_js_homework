fb67c4bf69bb08023edbd6244181a5fd8d6380bd
Marina11660088 <149905547+Marina11660088@users.noreply.github.com>
2024-02-19 11:53:25 +0200

    Add files via upload

---
+++ homework-m21.js
@@ -0,0 +1,46 @@
+// let name = "Marina";
+// let city = "Kiev";
+// city = name;
+// console.log(city)
+
+
+//  let name = "Olga";
+//  console.log(`привіт ${1}`); //привіт 1
+// console.log(`привіт ${"name"}`); //привіт name
+//  console.log(`привіт ${name}`); //Привіт Olga
+
+
+// let a = "5";
+// console.log(typeof Number(a));
+// let b = "13cvb";
+// console.log(typeof parseInt(b));
+// let c = "12.9sxdcfgv";
+// console.log(typeof parseFloat(c));
+
+
+// console.log((0.1*10+0.2*10)/10);
+
+// console.log(Math.max(20,10,50,40));
+
+// console.log(Math.random()*(4-2)+2);
+
+// const message = "Welcome to Bahamas!";
+// console.log(message.length);//19
+
+// console.log(message.toUpperCase());
+
+// let user={};
+// user.name="Marina";
+// user.age=15;
+// user.city="Ontario";
+// // console.log(user);
+// delete user.city;
+// user["like flowers"]=true;
+// // console.log(user);
+
+
+// for (key in user){
+//     console.log(key);
+//     console.log(user[key]);
+// }
+
---
+++ homework-m22.js
@@ -0,0 +1,75 @@
+// let num=1;
+// num>0 ? console.log(true):console.log(false);//true
+// let num=0;
+// num>0 ? console.log(true):console.log(false);//false
+// let num=-3;
+// num>0 ? console.log(true):console.log(false);//false
+
+
+// let text="test";
+// text==="test" ? console.log(true):console.log(false);//true
+// let text="qwerty";
+// text==="test" ? console.log(true):console.log(false);//false
+// let text="true";
+// text==="test" ? console.log(true):console.log(false);//false
+
+
+// let num=1;
+// if (num>10) {
+//     console.log(num+5)
+// } else {
+//     console.log(false)
+// }
+//false
+// let num=10;
+// if (num>10) {
+//     console.log(num+5)
+// } else {
+//     console.log(false)
+// }
+//false
+// let num=13;
+// if (num>10) {
+//     console.log(num+5)
+// } else {
+//     console.log(false)
+// }
+//18
+
+
+// const text=prompt("Введіть поточній місяць");
+// if (text==="січень") {
+//     console.log ("1")
+// } else if (text==="лютий") {
+//     console.log ("2")
+// } else if (text==="березень") {
+//     console.log ("3")
+// } else if (text==="квітень") {
+//     console.log ("4")
+// } else if (text==="травень") {
+//     console.log ("5")
+// } else if (text==="червень") {
+//     console.log ("6")
+// } else if (text==="липень") {
+//     console.log ("7")
+// } else if (text==="серпень") {
+//     console.log ("8")
+// } else if (text==="вересень") {
+//     console.log ("9")
+// } else if (text==="жовтень") {
+//     console.log ("10")
+// } else if (text==="листопад") {
+//     console.log ("11")
+// } else if (text==="грудень") {
+//     console.log ("12")
+// } else {
+//     console.log ("Введіть інші дані");
+// }
+
+
+// const numb=prompt ("Введіть трізначне число");
+// let a=(numb%10);
+// let b=(((numb-a)/10)%10);
+// let ba=(b*10+a)
+// let c=((numb-ba)/100)
+// console.log (a+b+c);
