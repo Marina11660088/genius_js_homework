086b68712d4899a33a40ab8b2449b971f4ba74ef
Marina11660088 <149905547+Marina11660088@users.noreply.github.com>
2024-02-21 21:23:45 +0200

    Add files via upload

---
+++ homework-m23.js
@@ -0,0 +1,40 @@
+// const fruts = [
+//     { id: 0, name: "Apple" },
+//     { id: 1, name: "Tomat" },
+//     { id: 2, name: "Cherry" },
+//     { id: 3, name: "Orange" },
+// ];
+// let names = fruts.map(item => item.name);
+// console.log (names);
+
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//       alert( i );
+//     }
+//   }
+
+
+// let i=-1;
+// while (i<4) {
+//     i++;
+//     console.log (`цифра ${i}!`);
+// }
+
+
+// let num;
+// do {
+//   num = prompt("Введите число больше 100", 0);
+// } while (num <= 100 && num);
+
+
+// const girls = [
+// { age: 23, name: "Оля" },
+// { age: 29, name: "Аня" },
+// { age: 10, name: "Юля" },
+// { age: 20, name: "Катя" },
+// ];
+// let arr=girls.map((item)=>item.age);
+// console.log(arr);
+// let summ=arr.reduce((sum,item)=>sum+item)/arr.length;
+// console.log(summ);
