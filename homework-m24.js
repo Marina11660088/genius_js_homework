e4f973115eae5c0aa805e0e2b94812f8695ee591
Marina11660088 <149905547+Marina11660088@users.noreply.github.com>
2024-03-02 15:50:45 +0200

    Add files via upload

---
+++ homework-m24.js
@@ -0,0 +1,49 @@
+// Перепишіть функцію, використовуючи '?' або '||'
+
+
+// function checkAge(age) {
+//     return (age > 18) ? true : confirm('Батьки дозволили?');
+//   };
+//   checkAge(12)
+
+
+
+// Напишіть функцію min(a, b), яка повертає менше з двох чисел a та b
+
+// function min(a, b) {
+//     if (a < b) {
+//       return a;
+//     } else {
+//       return b;
+//     }
+//   };
+ 
+
+
+
+// Перепишіть з використанням стрілкових функцій
+// Замініть Функціональні Вирази на стрілкові функції у коді нижче:
+
+
+// function ask(question, yes, no) {
+// if (confirm(question)) yes();
+// else no();
+// }
+// ask(
+// "Ви згодні?"
+// ,
+// function() { alert("Ви погодились."); },
+// function() { alert("Ви скасували виконання."); }
+// );
+
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes();
+//     else no();
+//   }
+  
+//   ask(
+//     "Ви згодні?",
+//     () => alert("Ви погодились."),
+//     () => alert("Ви скасували виконання.")
+//   );
